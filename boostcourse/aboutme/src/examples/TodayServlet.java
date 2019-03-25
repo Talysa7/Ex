ee0a03ac92b11e717bc11c489322d09091b12530
Lazy Talysa <42195207+Talysa7@users.noreply.github.com>
2019-03-26 01:19:22 +0900

    Update to use formatter
    And removed useless comments!

--- boostcourse/aboutme/src/examples/TodayServlet.java
+++ boostcourse/aboutme/src/examples/TodayServlet.java
@@ -3,6 +3,8 @@ package examples;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,26 +15,18 @@ import javax.servlet.http.HttpServletResponse;
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public TodayServlet() {
-        super();
-    }
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
-		LocalDateTime time = LocalDateTime.now();
-		//Lines were separated to read easier
+		String formedTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
+		
 		out.print("<html>");
 		out.print("<head><title>today</title></head>");
 		out.print("<body>");
-		//Hyperlink to the index
 		out.print("<a href='/aboutme/index.html'>메인화면</a>");
 		out.print("<h1><center>");
 		out.print("<br><br>");
-		//Print local time, format : '현재시간 : yyyy/mm/dd hh:mm'
-		out.print("현재시간 : "
-				+ time.getYear() + "/" + time.getMonthValue() + "/" + time.getDayOfMonth() + " " 
-				+ time.getHour() + ":" + time.getMinute());
+		out.print("현재시간: " + formedTime);
 		out.print("</h1></center>");
 		out.print("</body>");
 		out.print("</html>");
